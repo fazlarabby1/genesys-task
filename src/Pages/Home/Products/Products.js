186012c68f6363cdca134bb044015609910849b5
fazlarabby1 <fazlarabby41@gmail.com>
2023-02-10 21:33:46 +0600

    added products section

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -11,26 +11,26 @@ const Products = () => {
     const productData = [
         {
             id: 1,
-            img: "DK",
+            image: product1,
             title: 'Rogue HG 2.0 Bumper Plates',
             price: '$409',
             newPrice: '$300'
         },
         {
             id: 2,
-            img: "DK",
+            image: product2,
             title: 'The Ohio Bar - Cerakote',
             price: '$300'
         },
         {
             id: 3,
-            img: "DK",
+            image: product3,
             title: 'Rogue Adjustable Bench 3.0',
             price: '$300'
         },
         {
             id: 4,
-            img: "DK",
+            image: product4,
             title: 'Rogue USA Nylon Lifting Belt',
             price: '$60',
             newPrice: '$30'
@@ -41,12 +41,18 @@ const Products = () => {
             <div className='flex justify-center'>
                 <img src={whatsImg} alt="" />
             </div>
-            <h1 className='font-bold text-[56px] text-center'>SHOP THE LATEST</h1>
+            <div className='mb-[72px]'>
+                <h1 className='font-bold text-[56px] text-center'>SHOP THE LATEST</h1>
+                <p className='text-center text-gray-500 w-3/6 mx-auto'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis Theme natoque penatibus et magnis dis parturient montes, nascetur</p>
+            </div>
 
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
                 {
                     productData.map(product => <ProductCard key={product.id} product={product}></ProductCard>)
                 }
+            </div>
+            <div className='flex justify-center mt-16 mb-24'>
+                <button className='btn btn-outline rounded-none'>View All Products</button>
             </div>
         </div>
     );
